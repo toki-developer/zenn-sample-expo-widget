b7965fcb4660f29d51e7a6165058ac263ea6f147
Toki <55911995+toki-developer@users.noreply.github.com>
2025-11-30 23:12:47 +0900

    Add SQLite support and initial database setup; update dependencies

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,24 +1,65 @@
-import { DarkTheme, DefaultTheme, ThemeProvider } from '@react-navigation/native';
-import { Stack } from 'expo-router';
-import { StatusBar } from 'expo-status-bar';
-import 'react-native-reanimated';
+import {
+  DarkTheme,
+  DefaultTheme,
+  ThemeProvider,
+} from "@react-navigation/native";
+import { Stack } from "expo-router";
+import { SQLiteProvider, type SQLiteDatabase } from "expo-sqlite";
+import { StatusBar } from "expo-status-bar";
+import "react-native-reanimated";
 
-import { useColorScheme } from '@/hooks/use-color-scheme';
+import { useColorScheme } from "@/hooks/use-color-scheme";
 
 export const unstable_settings = {
-  anchor: '(tabs)',
+  anchor: "(tabs)",
 };
 
 export default function RootLayout() {
   const colorScheme = useColorScheme();
 
   return (
-    <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
-      <Stack>
-        <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
-        <Stack.Screen name="modal" options={{ presentation: 'modal', title: 'Modal' }} />
-      </Stack>
-      <StatusBar style="auto" />
+    <ThemeProvider value={colorScheme === "dark" ? DarkTheme : DefaultTheme}>
+      <SQLiteProvider databaseName="database.db" onInit={migrateDbIfNeeded}>
+        <Stack>
+          <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
+          <Stack.Screen
+            name="modal"
+            options={{ presentation: "modal", title: "Modal" }}
+          />
+        </Stack>
+        <StatusBar style="auto" />
+      </SQLiteProvider>
     </ThemeProvider>
   );
+}
+
+async function migrateDbIfNeeded(db: SQLiteDatabase) {
+  const DATABASE_VERSION = 1;
+  let currentDbVersion;
+  let res = await db.getFirstAsync<{
+    user_version: number;
+  }>("PRAGMA user_version");
+  currentDbVersion = res ? res.user_version : 0;
+  if (currentDbVersion >= DATABASE_VERSION) {
+    return;
+  }
+  if (currentDbVersion === 0) {
+    // テーブル作成
+    await db.execAsync(`
+      CREATE TABLE sample (
+        id INTEGER PRIMARY KEY,
+        name TEXT NOT NULL
+      );
+    `);
+
+    // 初期データ挿入
+    await db.execAsync(`
+      INSERT INTO sample (id, name) VALUES
+      (1, 'hoge'),
+      (2, 'halloWidget');
+    `);
+
+    currentDbVersion = 1;
+  }
+  await db.execAsync(`PRAGMA user_version = ${DATABASE_VERSION}`);
 }
